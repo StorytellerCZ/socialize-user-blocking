bd393842ce0fb4b3bf5561c572b3446b7c2096ac
Jan Dvorak <jdvorak@literaryuniverse.com>
2024-05-05 00:21:57 +0200

    Initial Meteor 3 work

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 /* global Package*/
 Package.describe({
     name: 'socialize:user-blocking',
-    version: '1.0.6',
+    version: '2.0.0',
     summary: 'Allow users to block each other',
     git: 'https://github.com/copleykj/socialize-user-blocking.git',
 });
 
 Package.onUse(function _(api) {
-    api.versionsFrom(['1.10.2', '2.3']);
+    api.versionsFrom(['2.8.1', '3.0-rc.0']);
     api.use([
         'check',
-        'reywood:publish-composite@1.7.3',
-        'socialize:user-model@1.0.5',
+        'reywood:publish-composite@1.8.9',
+        'socialize:user-model@2.0.0',
     ]);
     api.imply('socialize:user-model');
     api.mainModule('server/server.js', 'server');
